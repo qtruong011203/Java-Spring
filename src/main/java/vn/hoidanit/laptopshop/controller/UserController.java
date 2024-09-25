85681ebb82787a9935cbde552a7636f9399c1ff0
pqtruong0112 <quangtruong01122003@gmail.com>
2024-09-25 23:51:21 +0700

    update 25/09

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,31 +2,31 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import vn.hoidanit.laptopshop.model.User;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
 
-        model.addAttribute("pqt", test);
-        model.addAttribute("qt0112", "java spring booot");
-        return "hello";
+        model.addAttribute("pqt", "test");
+        model.addAttribute("qt0112", "spring");
+        return "hello1";
     }
 
     @RequestMapping("/admin/user")
@@ -38,7 +38,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User pqt) {
         System.out.println("run here" + pqt);
-
+        this.userService.handleSaveUser(pqt);
         return "hello";
     }
 }
